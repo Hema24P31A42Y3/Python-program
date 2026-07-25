2caf8d72220c60fa6886c1954c28c5533dc4fe30
b.k.hemasitha <hemasitha20@gmail.com>
2026-07-25 22:45:01 +0530

    Add script to find duplicate numbers in a list

---
+++ Duplicate.py
@@ -0,0 +1,9 @@
+nums = list(map(int, input().split()))
+
+duplicates = []
+
+for num in nums:
+    if nums.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
+
+print(duplicates)
