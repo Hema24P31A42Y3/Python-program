9321728656b39a1a324080a383a39782367652ec
b.k.hemasitha <hemasitha20@gmail.com>
2026-09-25 20:22:14 +0530

    Create school.py

---
+++ school.py
@@ -0,0 +1,28 @@
+class HemaSchool():
+      school_name="Hemaa School"
+      school_location="akhividu"
+      grade=6
+      fee=50000
+      def student_details(self,name,dob,parent,location):
+          self.name=name
+          self.dob=dob
+          self.parent=parent
+          self.location=location
+          print("name:",name,"date of birth:",dob,"parent:",parent,"location:",location)
+suni=HemaSchool()
+vijju=HemaSchool()
+siddu=HemaSchool()
+prakash=HemaSchool()
+print(suni.school_location)
+print(vijju.school_location)
+print(siddu.school_location)
+print(prakash.school_location)
+print(suni.grade)
+print(vijju.grade)
+print(siddu.grade)
+print(prakash.grade)
+print(suni.fee)
+print(vijju.fee)
+print(siddu.fee)
+print(prakash.fee)
+        
