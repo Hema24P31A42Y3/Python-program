5e0e9c80b7fb38e7f6e12c2132403d121ad93310
b.k.hemasitha <hemasitha20@gmail.com>
2026-09-25 21:03:40 +0530

    Create vehical.py

---
+++ vehical.py
@@ -0,0 +1,17 @@
+class Vehical():
+      bike="pulsur"
+      car="breeza"
+      auto="mahendra"
+      def Owner(self,name,village,vehical):
+          self.name=name
+          self.village=village
+          self.vehical=vehical
+          print(f"""
+          name={name}
+          village={village}
+          vehical={vehical}
+          """)
+sid=Vehical()
+raaji=Vehical()
+venkii=Vehical()
+raaji.Owner("raaji","ktlk","breeza")
